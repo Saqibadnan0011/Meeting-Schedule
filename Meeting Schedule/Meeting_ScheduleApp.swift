48f2da2dbadab9ff988d224a4f47f474f1bf98ff
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-10-29 22:25:13 +0500

    Initial Commit

---
+++ Meeting Schedule/Meeting_ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meeting_ScheduleApp.swift
+//  Meeting Schedule
+//
+//  Created by apple on 29/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Meeting_ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
