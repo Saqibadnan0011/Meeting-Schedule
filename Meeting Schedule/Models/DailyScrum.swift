3bad3ef93a67dc6ced1e739460a2df2290b1b58a
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-10-30 01:05:36 +0500

    add id initialiser in dailyscrum

--- Meeting Schedule/Models/DailyScrum.swift
+++ Meeting Schedule/Models/DailyScrum.swift
@@ -7,31 +7,40 @@
 
 import Foundation
 
-struct DailyScrum {
+struct DailyScrum: Identifiable {
+    var id: UUID
     var title: String
     var attendees: [String]
     var lengthInMinutes: Int
     var theme: Theme
+    
+    init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
+        self.id = id
+        self.title = title
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+        self.theme = theme
+    }
 }
 
 extension DailyScrum {
     static let sampleData: [DailyScrum] =
     [
         DailyScrum(title: "iOS Dev",
-                   attendees: ["Saqib", "Adnan", "David"],
+                   attendees: ["Saqib", "Adnan", "David", "Ali", "Angel"],
                    lengthInMinutes: 10,
                    theme: .yellow),
         DailyScrum(title: "Mobile App Dev",
-                   attendees: ["Saqib", "Adnan", "David"],
+                   attendees: ["Saqib", "Adnan", "David", "Mike"],
                    lengthInMinutes: 5,
                    theme: .orange),
         DailyScrum(title: "Ai/ML",
-                   attendees: ["Saqib", "Adnan", "David"],
+                   attendees: ["Saqib", "Adnan", "David", "Jay"],
                    lengthInMinutes: 5,
                    theme: .poppy),
-        //DailyScrum(title: "Software Tester",
-                   //attendees: ["Saqib", "Adnan", "David"],
-                   //lengthInMinutes: 10,
-                   //theme: .periwinkle)
+        DailyScrum(title: "Software Tester",
+                   attendees: ["Saqib", "Adnan", "David" ],
+                   lengthInMinutes: 10,
+                   theme: .periwinkle)
     ]
 }
