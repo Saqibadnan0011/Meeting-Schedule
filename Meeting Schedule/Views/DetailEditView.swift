b2cdd51278dc1d6a20513d3be71bd80e75b39ea5
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-10-31 23:28:54 +0500

    Add new Attendees  and func

--- Meeting Schedule/Views/DetailEditView.swift
+++ Meeting Schedule/Views/DetailEditView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct DetailEditView: View {
     
     @State private var scrum = DailyScrum.emptyScrum
+    @State private var newAttendeeName = ""
     
     var body: some View {
         Form {
@@ -18,6 +19,22 @@ struct DetailEditView: View {
                 HStack {
                     Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
                         Text("Length")
+                    }
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) Minutes")
+                }
+            }
+            Section(header: Text("Attendee")) {
+                ForEach (scrum.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete { indices in
+                    scrum.attendees.remove(atOffsets: indices)
+                }
+                HStack {
+                    TextField("New Attendee", text: $newAttendeeName)
+                    Button(action: {}) {
+                        Image(systemName: "plus.circle.fill")
                     }
                 }
             }
