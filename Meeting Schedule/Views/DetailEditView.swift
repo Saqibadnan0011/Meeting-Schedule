a0a2d5d6ca5c6bee7ff710618df217df7b2fee81
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-10-31 23:01:38 +0500

    Created new editview

---
+++ Meeting Schedule/Views/DetailEditView.swift
@@ -0,0 +1,23 @@
+//
+//  DetailEditView.swift
+//  Meeting Schedule
+//
+//  Created by apple on 31/10/2023.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    
+    @State private var scrum = DailyScrum.emptyScrum
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
