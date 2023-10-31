46b8c5a11ca05cd292333e94b49dd6abe2763853
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-10-31 22:34:09 +0500

    Navigate different screens

--- Meeting Schedule/Views/DetailView.swift
+++ Meeting Schedule/Views/DetailView.swift
@@ -13,9 +13,11 @@ struct DetailView: View {
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
-                Label("Start meeting", systemImage: "timer")
-                    .font(.headline)
-                    .foregroundColor(.accentColor)
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
                 HStack {
                     Label("Length", systemImage: "clock")
                     Spacer()
@@ -24,9 +26,22 @@ struct DetailView: View {
                 .accessibilityElement(children: .combine)
                 HStack {
                     Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.mainColor)
+                        .background(scrum.theme.accentColor)
+                        .cornerRadius(4)
+                }
+                .accessibilityElement(children: .combine)
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
                 }
             }
         }
+        .navigationTitle(scrum.title)
     }
 }
 
